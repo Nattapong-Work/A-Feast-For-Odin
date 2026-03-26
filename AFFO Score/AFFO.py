c50ed69f478c17977f31b86c1738b6d308fff534
Nattapong <work.nattapong2020@gmail.com>
2026-03-26 19:26:44 +0700

    Initial commit

---
+++ AFFO Score/AFFO.py
@@ -0,0 +1,33 @@
+score = {"whaling_ship" : 3,
+         "knarr" : 5,
+         "longship" : 8,
+         "shed" : 5,
+         "house" : 3,
+         "longhouse" : 2,
+         "knarr_emi" : 2,
+         "longship_emi" : 2,
+         "sheep" : 2,
+         "cattle" : 2,
+         }
+
+exploration = {"baffin" : {"score" : 12, "start" : -24},
+               "bare" : {"score" : 12, "start" : -22},
+               "faroe" : {"score" : 4, "start" : -16},
+               "greenland" : {"score" : 12, "start" : -20},
+               "iceland" : {"score" : 16, "start" : -24},
+               "labrador" : {"score" : 36, "start" : -40},
+               "newfoundland" : {"score" : 38, "start" : -40},
+               "shetland" : {"score" : 6, "start" : -24},
+               }
+
+emigration = 13
+pregnant = 1
+english_crown = 2
+occupation = 0
+silver = 0
+income = 0
+
+max_whale = 3
+max_knarr_longship = 3
+
+total_score = 0
