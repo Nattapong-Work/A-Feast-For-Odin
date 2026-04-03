68a8c7456900441dd4e92c9b00611554804e95f6
Nattapong <work.nattapong2020@gmail.com>
2026-04-03 17:44:50 +0700

    feat : Create the main function for calculations, a calculate button, and a reset button.

--- AFFO Score/AFFO_Logic.py
+++ AFFO Score/AFFO_Logic.py
@@ -1,44 +1,66 @@
-def get_final_score() -> int :
-    final_score = all_sum() - all_min()
-    return final_score
+class FinalScore():
 
-def all_sum() -> int:
-    final_score = (
-        ship_score() +
-        exploration_score() +
-        building_score() +
-        animal_score()
-    )
-    return final_score
+    @staticmethod
+    def get_final_score(
+            whale, knarr, long_ship, knarr_emi, long_emi, silver, income, occupation, shed, shed_minus,
+            house, house_minus, longhouse, longhouse_minus, sheep, cattle, sheep_preg, cattle_preg,
+            exploration_minus, village_minus
+    ) -> int :
+        score = (FinalScore.ship_score(whale, knarr, long_ship, knarr_emi, long_emi,) +
+                 FinalScore.building_score(shed, house, longhouse) +
+                 FinalScore.all_sum(silver, income, occupation) +
+                 FinalScore.animal_score(sheep, cattle, sheep_preg, cattle_preg) -
+                 FinalScore.all_min(shed_minus, house_minus, longhouse_minus,exploration_minus, village_minus))
 
-def all_min() -> int :
-    pass
+        return score
 
-def ship_score(whale, knarr, long_ship) -> int:
-    final_score = (
-        (whale * 3) +
-        (knarr * 5) +
-        (long_ship * 8)
-    )
-    return final_score
 
-def exploration_score() -> int:
-    pass
+    @staticmethod
+    def all_sum(silver, income, occupation) -> int:
+        score = (silver + income + occupation)
+        print(f"all sum : {score}")
+        return score
 
-def building_score(shed, house, long_house) -> int:
-    final_score = (
-        (shed * 8) +
-        (house * 10) +
-        (long_house * 17)
-    )
-    return final_score
+    @staticmethod
+    def all_min(shed_minus, house_minus, longhouse_minus, exploration_minus, village_minus) -> int :
+        score = (shed_minus + house_minus + longhouse_minus + exploration_minus + village_minus)
+        print(f"all min : {score}")
+        return score
 
-def animal_score(sheep, sheep_preg, cattle, cattle_preg) -> int:
-    final_score = (
-        sheep_preg +
-        cattle_preg +
-        (sheep * 2) +
-        (cattle * 3)
-    )
-    return final_score
+    @staticmethod
+    def ship_score(whale, knarr, long_ship, knarr_emi, long_emi,) -> int:
+        score = (
+            (whale * 3) +
+            (knarr * 5) +
+            (long_ship * 8) +
+            (knarr_emi * 18) +
+            (long_emi * 21)
+        )
+        print(f"ship : {score}")
+        return score
+
+    @staticmethod
+    def exploration_score() -> int:
+        pass
+
+    @staticmethod
+    def building_score(shed, house, long_house) -> int:
+        score = (
+            (shed * 8) +
+            (house * 10) +
+            (long_house * 17)
+        )
+        print(f"building : {score}")
+        return score
+
+    @staticmethod
+    def animal_score(sheep, sheep_preg, cattle, cattle_preg) -> int:
+        score = (
+            sheep_preg +
+            cattle_preg +
+            (sheep * 2) +
+            (cattle * 3)
+        )
+        print(f"animal : {score}")
+        return score
 
--- AFFO Score/main.py
+++ AFFO Score/main.py
@@ -1,12 +1,95 @@
 import sys
 from PyQt6 import QtWidgets
 from AFFO_main_ui import MyWindow
-import AFFO_Logic as logic
+from AFFO_Logic import FinalScore
 
 
 class AppController(MyWindow):
     def __init__(self):
         super().__init__()
+
+
+        self.pb_done.clicked.connect(self.get_final_score)
+        self.pb_reset.clicked.connect(self.reset)
+
+
+    def get_island(self, state, points):
+        pass
+
+
+    def get_final_score(self):
+        # โค้ดส่วนที่รวมรวมค่าจาก สปินบ็อกอื่นๆ มา
+        data = {
+            "whale" : self.spb_whale.value(),
+            "knarr" : self.spb_knarr.value(),
+            "long ship" : self.spb_long.value(),
+            "knarr emi" : self.spb_knarr_emi.value(),
+            "long emi" : self.spb_long_emi.value(),
+            "silver" : self.spb_sil.value(),
+            "income" : self.spb_inc.value(),
+            "occupation" : self.spb_occu.value(),
+            "shed" : self.spb_shed_num.value(),
+            "shed minus" : self.spb_shed_mi.value(),
+            "house" : self.spb_hou_num.value(),
+            "house minus" : self.spb_hou_mi.value(),
+            "long house" : self.spb_lng_num.value(),
+            "long house minus" : self.spb_lng_mi.value(),
+            "sheep" : self.spb_shp.value(),
+            "preg sheep" : self.spb_preg_shp.value(),
+            "cattle" : self.spb_cattle.value(),
+            "preg cattle" : self.spb_preg_cattle.value(),
+            "exploration minus" : self.spb_mi_expl.value(),
+            "main village minus" : self.spb_vill_mi.value(),
+        }
+
+        # เรียกใช้ฟังก์ชั่นที่อยู่ในลอจิก พร้อมค่าพรารามิเตอร์ ที่เปนค่าที่ต้องคำนวณเช่น goo(ship = 20, house = 30,...) return กลับมาในตัวแปร
+        total_score = FinalScore.get_final_score(
+            data["whale"],
+            data["knarr"],
+            data["long ship"],
+            data["knarr emi"],
+            data["long emi"],
+            data["silver"],
+            data["income"],
+            data["occupation"],
+            data["shed"],
+            data["shed minus"],
+            data["house"],
+            data["house minus"],
+            data["long house"],
+            data["long house minus"],
+            data["sheep"],
+            data["preg sheep"],
+            data["cattle"],
+            data["preg cattle"],
+            data["exploration minus"],
+            data["main village minus"]
+        )
+        # ใช้ self.lb_total_scr.set text เปนตัวแปรที่โค้แถวบนคนืนค่ามา
+        self.lb_total_scr.setText(str(total_score))
+
+    def reset(self):
+        self.spb_whale.setValue(0)
+        self.spb_knarr.setValue(0)
+        self.spb_long.setValue(0)
+        self.spb_knarr_emi.setValue(0)
+        self.spb_long_emi.setValue(0)
+        self.spb_sil.setValue(0)
+        self.spb_inc.setValue(0)
+        self.spb_occu.setValue(0)
+        self.spb_shed_num.setValue(0)
+        self.spb_shed_mi.setValue(0)
+        self.spb_hou_num.setValue(0)
+        self.spb_hou_mi.setValue(0)
+        self.spb_lng_num.setValue(0)
+        self.spb_lng_mi.setValue(0)
+        self.spb_shp.setValue(0)
+        self.spb_preg_shp.setValue(0)
+        self.spb_cattle.setValue(0)
+        self.spb_preg_cattle.setValue(0)
+        self.spb_mi_expl.setValue(0)
+        self.spb_vill_mi.setValue(0),
+
 
 
 
