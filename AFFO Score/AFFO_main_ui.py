0d5fc083e80f3b40b3a96a48b9a3c44ed13fd946
Nattapong <work.nattapong2020@gmail.com>
2026-04-07 15:24:18 +0700

    chore : Add UI and program icon files, and fix the paths.

--- AFFO Score/AFFO_main_ui.py
+++ AFFO Score/AFFO_main_ui.py
@@ -4,7 +4,7 @@ from PyQt6 import QtWidgets, uic, QtGui
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:/Users/Nuttapon/OneDrive/Desktop/G@nn3/Not a game/QT/AFFO_main_window.ui", self)
+        uic.loadUi("AFFO_src/AFFO_main_window.ui", self)
         self.setWindowTitle("AFFO Score")
-        self.setWindowIcon(QtGui.QIcon("C:/Users/Nuttapon/OneDrive/Desktop/G@nn3/PICPIC/AFFO_icon.png"))
+        self.setWindowIcon(QtGui.QIcon("AFFO_src/AFFO_icon.png"))
 
